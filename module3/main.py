32f55741cbae31f738363b7fdbde7b7d14faf9d6
wilvillanueva <wilfredopvillanueva@gmail.com>
2015-08-13 10:00:41 +0800

    MODULE 3

---
+++ module3/main.py
@@ -0,0 +1,88 @@
+import os
+import cgi
+import urllib
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import logging
+from google.appengine.ext.webapp import template
+import jinja2
+import webapp2
+import json
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+##########################NEW THESIS ENTRY#######################################
+
+class thesisEntry(ndb.Model):
+    Title = ndb.StringProperty(indexed = True)
+    Adviser = ndb.StringProperty(indexed = True)
+    Abstract = ndb.StringProperty(indexed = True)
+    Year = ndb.IntegerProperty(indexed = True)
+    Section = ndb.IntegerProperty(indexed = True)
+    Date = ndb.DateTimeProperty(auto_now_add = True)
+
+class MainPageHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('main.html')
+        self.response.write(template.render())
+
+    def post(self):
+    	thesis = thesisEntry()
+    	thesis.Title = self.request.get('thesis_title')
+    	thesis.Adviser = self.request.get('thesis_adviser')
+    	thesis.Abstract = self.request.get('thesis_abstract')
+    	thesis.Year = int(self.request.get('thesis_year'))
+    	thesis.Section = int(self.request.get('thesis_section'))
+    	thesis.put()
+
+class apiThesis(webapp2.RequestHandler):
+    def post(self):
+    	thesis = thesisEntry()
+    	thesis.Title = self.request.get('thesis_title')
+    	thesis.Adviser = self.request.get('thesis_adviser')
+    	thesis.Abstract = self.request.get('thesis_abstract')
+    	thesis.Year = int(self.request.get('thesis_year'))
+    	thesis.Section = int(self.request.get('thesis_section'))
+    	thesis.put()
+        self.response.headers['Content-Type'] = 'app/json'
+        response = {
+            'result': 'OK',
+            'data': {
+                'id':thesis.key.urlsafe(),
+                'thesis_title': thesis.Title,
+                'thesis_year': thesis.Year,
+                'thesis_abstract': thesis.Abstract,
+                'thesis_adviser': thesis.Adviser,
+                'thesis_section': thesis.Section
+            }
+        }
+        self.response.out.write(json.dumps(response))
+
+    def get(self):
+        thesiss = thesisEntry.query().order(-thesisEntry.Date).fetch()
+        thesis_list = []
+        for thesis in thesiss:
+            thesis_list.append({
+                'id':thesis.key.urlsafe(),
+                'thesis_title': thesis.Title,
+                'thesis_year': thesis.Year,
+                'thesis_abstract': thesis.Abstract,
+                'thesis_adviser': thesis.Adviser,
+                'thesis_section': thesis.Section
+                })
+        response = {
+            'results': 'OK',
+            'data': thesis_list
+        }
+        self.response.headers['Content-Type'] = 'app/json'
+        self.response.out.write(json.dumps(response))
+
+
+app = webapp2.WSGIApplication([
+	('/', MainPageHandler),
+    ('/home', MainPageHandler),
+    ('/api/thesis', apiThesis)
+], debug=True)
---
+++ module3/static/script.js
@@ -0,0 +1,37 @@
+$(function() {
+    function onFormSubmit(event) {
+        var data = $(event.target).serializeArray();
+        var thesis = {};
+        for (var i = 0; i < data.length; i++) {
+            var key = data[i].name;
+            var value = data[i].value;
+            thesis[key] = value
+        }
+        var list_element = $('<li>');
+        list_element.html("Title: " + thesis.thesis_title + ' Year: ' + thesis.thesis_year + ' Abstract: ' + thesis.thesis_abstract + ' Adviser: ' + thesis.thesis_adviser + ' Section: ' + thesis.thesis_section);
+        $('.thesis-list').prepend(list_element);
+
+        var thesis_entry_api = '/api/thesis';
+        $.post(thesis_entry_api, thesis, function(response){
+            if (response.status = 'OK'){
+                list_element.html(thesis.thesis_year + ' ' +  thesis.thesis_title);
+            }
+        });
+
+        return false;
+    }
+    $('.create-form').submit(onFormSubmit)
+
+    function loadThesis(event){
+        var thesis_list_api = '/api/thesis';
+        $.get(thesis_list_api, {}, function(response){
+            console.log('thesis_list', response);
+            response.data.forEach(function(thesis){
+            	var list_element = $('<li>');
+        		list_element.html(thesis.thesis_year + ' ' + thesis.thesis_title);
+        		$('.thesis-list').prepend(list_element);
+                })
+        });
+    }
+    loadThesis();
+});
